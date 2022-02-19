b7afce883231d47a5ac9d95cd33499e61d3e8edf
unaif53 <jyckss12@ewhain.net>
2022-02-19 21:55:12 +0900

    update

--- js/clock.js
+++ js/clock.js
@@ -4,12 +4,12 @@ const day = document.querySelector("div #date");
 function getClock(){
     const date = new Date();
     const years = String(date.getFullYear());
-    const months = String(date.getMonth());
-    const days = String(date.getDay());
+    const months = String(date.getMonth()+1);
+    const days = String(date.getDate());
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
-    day.innerText = `${months} / ${days} / ${years}`;
+    day.innerText = `${years} / ${months} / ${days}`;
     time.innerText = `${hours} : ${minutes} : ${seconds}`;
 }
 getClock();
